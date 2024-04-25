5cd29bf3b58177579f72df23b16e45c2b49cfc9d
Onurubu <onurubu@gmail.com>
2024-04-25 13:04:07 +0200

    Initial commit with data and code to begin preapring raw data for further analyses.

---
+++ oct_2022_beetles_analysis.R
@@ -0,0 +1,3 @@
+#### Coskun Kucukkaragoz
+### 25th of April 2024
+## Start of the R script to analyse the data from my mounting, pinning, and sorting, of beetles for the October 2022 season from the Cederburg
